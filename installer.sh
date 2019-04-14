9c6c47da970f379f14dda66cc126a9fabbb91170
Mike Saunders <mikejs@techiemike.com>
2019-04-14 15:51:43 +0700

    updated installer

--- installer.sh
+++ installer.sh
@@ -18,7 +18,7 @@ apt-get update # Update repos
 
 if [[ -n $(cat /etc/os-release |grep kali) ]]
 then
-	apt-get install libservlet3.0-java 
+	apt-get install libservlet3.0-java
 	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
 	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
 	apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
@@ -29,7 +29,7 @@ apt-get install -y i2p-keyring #this will ensure you get updates to the reposito
 apt-get install -y secure-delete tor i2p # install dependencies, just in case
 
 # Configure and install the .deb
-dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
-dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages
-
+dpkg-deb -b ubuntu-anonsurf-deb-src/ ububtu-anonsurf.deb # Build the deb package
+dpkg -i ubuntu-anonsurf.deb || (apt-get -f install && dpkg -i ububtu-anonsurf.deb) # this will automatically install the required packages
+sudo chown sub7:sub7 ububtu-anonsurf.deb
 exit 0
